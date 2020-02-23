e33b98493d7ab4770a8811105eb7520478e40298
Arnouts <carnout@fbinsmi.com>
2020-02-23 12:13:30 -0500

    commit on 2/23/20

---
+++ Forecasting_Chapter_2_HW.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+library(forecast)
+
+## use the autoplot function
+autoplot(gold)
+autoplot(woolyrnq)
+
+
+frequency(gold)
+frequency(woolyrnq)
---
+++ Generalized_Additive_Models.R
@@ -0,0 +1,15 @@
+####################################################################################################################
+###################################### Generalized Additive Models #################################################
+####################################################################################################################
+
+## Think of building blocks as bases
+
+## examples
+### polynomials 
+#### ability to fit non linear functions 
+### Fourier Bases
+### Wavelet
+### Regression Splines
+#### Knot points
+
+library(forecast)
